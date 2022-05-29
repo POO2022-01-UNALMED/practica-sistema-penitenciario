c0ec7db3d80fffdc1602d3ace860cae9f97dc5f9
GennerPineda <gepinedace@unal.edu.co>
2022-05-29 00:42:11 -0500

    .

--- proyecto JAVA/src/gestorAplicacion/departamentos/Prision.java
+++ proyecto JAVA/src/gestorAplicacion/departamentos/Prision.java
@@ -1,23 +1,30 @@
 package gestorAplicacion.departamentos;
 
-import java.util.List;
+
 import java.util.ArrayList;
 import gestorAplicacion.personal.*;
 import gestorAplicacion.bienes.*;
 
 public class Prision {
-
+	
+	
 	private final int numCeldas;
 	private final int numPatios;
-	private int numGuardias;
-	private int numReos;
-	private int numBuses;
+	private final int numGuardias;
+	private final int numReos;
+	private final int numBuses;
 	private final boolean genero; // 1 hombre, 0 mujer.
 	private final String localizacion;
 	private final String nombre;
-	private static int prisionesCreadas=0;
-	//Falta aun mucho, discutir sobre si se a�ade a Alcaide o aqu� muchas cosas
-
+	private static int prisionesCreadas;
+	private ArrayList<Reo> reos = new ArrayList<Reo>();
+	private ArrayList<Guardia> guardias = new ArrayList<Guardia>();
+	private ArrayList<String> historialReos = new ArrayList<String>();
+	private ArrayList<String> historialGuardias = new ArrayList<String>();
+	private ArrayList<String> historialTrabajos = new ArrayList<String>();
+	private ArrayList<String> historialPrision = new ArrayList<String>();
+	
+	
 	public Prision(int numCeldas, int numPatios, int numGuardias, int numReos, int numBuses, boolean genero,
 			String localizacion, String nombre) {
 		super();
@@ -30,60 +37,75 @@ public class Prision {
 		this.localizacion = localizacion;
 		this.nombre = nombre;
 	}
-	
+
+	public static int getPrisionesCreadas() {
+		return prisionesCreadas;
+	}
+
+	public static void setPrisionesCreadas(int prisionesCreadas) {
+		Prision.prisionesCreadas = prisionesCreadas;
+	}
+
+	public static ArrayList<Reo> getReos() {
+		return reos;
+	}
+
+	public static void setReos(ArrayList<Reo> reos) {
+		Prision.reos = reos;
+	}
+
+	public static ArrayList<Guardia> getGuardias() {
+		return guardias;
+	}
+
+	public static void setGuardias(ArrayList<Guardia> guardias) {
+		Prision.guardias = guardias;
+	}
+
+	public int getNumCeldas() {
+		return numCeldas;
+	}
+
+	public int getNumPatios() {
+		return numPatios;
+	}
+
 	public int getNumGuardias() {
 		return numGuardias;
 	}
 
-	public void setNumGuardias(int numGuardias) {
-		this.numGuardias = numGuardias;
-	}
-	
 	public int getNumReos() {
 		return numReos;
 	}
-	
-	public void setNumReos(int numReos) {
-		this.numReos = numReos;
-	}
-	
+
 	public int getNumBuses() {
 		return numBuses;
 	}
-	
-	public void setNumBuses(int numBuses) {
-		this.numBuses = numBuses;
-	}
-	
-	public static int getPrisionesCreadas() {
-		return prisionesCreadas;
-	}
-	
-	public static void setPrisionesCreadas(int prisionesCreadas) {
-		Prision.prisionesCreadas = prisionesCreadas;
-	}
-	
-	public int getNumCeldas() {
-		return numCeldas;
-	}
-	
-	public int getNumPatios() {
-		return numPatios;
-	}
-	
+
 	public boolean isGenero() {
 		return genero;
 	}
-	
+
 	public String getLocalizacion() {
 		return localizacion;
 	}
-	
+
 	public String getNombre() {
 		return nombre;
 	}
-
-
-
-
+	
+	public String historialListas() {
+		ArrayList<String> patioList = Patio.getHistorialPatio();
+		ArrayList<String> celdaList = Patio.getHistorialPatio();
+	}
+	
+	
+	
 }
+	
+	
+	
+	
+	
+	
+	

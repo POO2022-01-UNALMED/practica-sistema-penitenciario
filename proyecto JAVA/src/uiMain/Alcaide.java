ad7b59ae398338cd65b9ab59a3ea0edcf171c11f
GennerPineda <gepinedace@unal.edu.co>
2022-05-30 17:18:38 -0500

    .

--- proyecto JAVA/src/uiMain/Alcaide.java
+++ proyecto JAVA/src/uiMain/Alcaide.java
@@ -2,8 +2,6 @@ package uiMain;
 
 import java.util.List;
 
-import java.lang.reflect.Method; 
-import java.lang.reflect.Modifier; 
 import java.util.ArrayList;
 import java.util.Scanner;
 import gestorAplicacion.bienes.*;
@@ -13,69 +11,27 @@ import gestorAplicacion.personal.*;
 public class Alcaide {
 
 	public static void main(String[] args) {
+		Reo genner = new Reo("Genner",1001, true, 10);
+		Reo andres = new Reo("Andres",1002, true, 10);
+		Reo faidher= new Reo("Faidher",1003, true, 10);
+		Reo alejandro = new Reo("Alejandro",1001, true, 10);
 		
-		Prision ascaban = new Prision("Ascaban");
+		Prision azkaban = new Prision("Azkaban");
+		Prision torreOscura = new Prision("Torre Oscura");
 		
-		//---------------------------Evaluacion clase abstracta------------------------------------
+		ArrayList<Bus> listaBusesAzkaban = new ArrayList<Bus>();
+		for (int i = 0; i < 6; i++) {
+			String k = "A100"+i;
+			listaBusesAzkaban.add(new Bus(k,azkaban));
+		}
 		
-		//El objetivo puntual de Prision es generar todas las celdas, bibliotecas, gimnasios necesarios para que todods
-		//los presos puedan acceder a ellas, por tanto a modo de ejemplo se presentará un objeto biblio1 de la clase Patio que nace siendo una
-		//biblioteca para mostrar la correcta implemmentación de ua clase abstracta y de un metod abstracto.
+		ArrayList<Bus> listaBusesTorreOscura = new ArrayList<Bus>();
+		for (int i = 0; i < 6; i++) {
+			String k = "T100"+i;
+			listaBusesTorreOscura.add(new Bus(k,torreOscura));
+		}
 		
-		//----------------------------------------------------------------------------------------
-		
-		Patio biblio1 = new Biblioteca("Biblio1", ascaban); // Patio es una clase abstracta la cual es padre de Biblioteca
-		System.out.println(biblio1.getHistorialPatio()); //getHistorialPatio es un método abstracto de la clase Patio
-		
-		
-		//---------------------------Evaluacion interfaz-------------------------------------------
-		
-		Persona persona1 = new Guardia("Jaime", 10001); //Persona es una interfaz y Guardia es su hija
-		System.out.println(persona1.getNombre()); // getNombre() es un método de la interfaz Persona el cual es inplementado en la Guardia
-		
-		//--------------------------Evaluacion de Herencia ---------------------------------------
-		
-		Gimnasio gim1 = new Gimnasio("Gim1", ascaban);// Gimnasio es hijo de Patio
-		System.out.println(gim1.getCapacidad()); //Gimnasio no tiene el metodo getCapacidad(), pero dicho método existe en Patio y por tanto se hereda.
-		
-		//--------------------------Evaluacion Ligadura Dinámica---------------------------------
-	
-		Reo genner = new Reo("Genner",20001, true, 10, ascaban); //creacion del reo Genner
-		gim1.ingresarReos(genner); //ingreso de reo genner al gimnasio gim1
-		System.out.println(gim1.getCantidadReos()); // el método getCantidadReos() es un metodo no abstracto defnido en Patio y definido en gim1 con misma firma y salida, en Patio
-													// dicho método tiene establecido que retornará el entero 1, por otro lado en gimnasio dicho método retornará el tamaño del vector reos
-													// por ligadura dinámica se tendrá que este último es el que se ejecuta y como solo se ha ingresado un reo entonces este necesariamnete 
-													// nos retornará 1.
-		biblio1.ingresarGuardias((Guardia)persona1); //ingresamos a persona1 vista como Guardia
-		System.out.println(biblio1.getCantidadGuardias()); //Nuevamente, el método getCantidadGuardias() deberá retornar en Patio el valor idéntico a 0, pero al ser definido en Biblioteca
-													// entonces por ligadura dinámica retornará la cnatidad de guardias existentes en biblio1
-		
-		//-------------------------- Evaluacion uso de constante-------------------------------
-		
-		Bus bus1 = new Bus("ACM1PT", ascaban); // ACM1PT es una constante ya que será la placa del bus
-		//bus1.placa = ""; // si intentamos cambiar la placa nos generará un error ya que es final
-		
-		//
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	
-		
-		
-		
-		
-		
-		
-		
-		
+		//___________________________________FUNCIONALIDAD 1: HISTORIAL DE LAS PRISIONES________________________________________________
 		
 		
 	}

bb710f3c01b216f0299a8cb0eeaf1bdb3118c1f6
GennerPineda <gepinedace@unal.edu.co>
2022-05-29 23:41:15 -0500

    .

--- proyecto JAVA/src/uiMain/Alcaide.java
+++ proyecto JAVA/src/uiMain/Alcaide.java
@@ -10,30 +10,9 @@ import gestorAplicacion.personal.*;
 public class Alcaide {
 
 	public static void main(String[] args) {
-		Prision Cali = new Prision("Cali");
-		Reo r = new Reo("Andres", 101, true, 12, Cali);
-		Guardia g = new Guardia("Alonso", 01, Cali, Rango.oficial);
-		Celda celda = new Celda(999);
 		
-		g.meterReoCelda(r, celda);
-		
-		for (int i = 0; i < r.getPrision().getHistorialGuardias().size(); i++) {
-			System.out.println(r.getPrision().getHistorialGuardias().get(i));
-		}
-		
-		System.out.println(r.getCelda().getNumCelda());
-		for (int i = 0; i < r.getPrision().getHistorialGuardias().size(); i++) {
-			System.out.println(r.getPrision().getHistorialGuardias().get(i));
-		}
-		
-//		Trabajo.asignarTrabajoReo(r, OpcionTrabajo.Bibliotecario);
-//		System.out.println(r.getPrision().historialTrabajos.get(0));
-//		Trabajo.renunciarTrabajoReo(r);
-//		System.out.println(r.getPrision().historialTrabajos.get(1));
-//		
-//		for (int i = 0; i < r.getPrision().historialTrabajos.size(); i++) {
-//			System.out.println(r.getPrision().historialTrabajos.get(i));
-//		}
+		Prision ascaban = new Prision("ascaban");
+		System.out.println(ascaban.historialPrision());
 	}
 
 }
